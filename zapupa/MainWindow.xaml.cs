b96dc9238802c15460c7cfecf0936e6602c977b8
BoGyX <166905990+BoGyX@users.noreply.github.com>
2024-04-13 12:07:27 +0300

    Add files via upload

---
+++ zapupa/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace zapupa
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Button_Click1(object sender, RoutedEventArgs e)
+        {
+            ItalianoMenu ItalianoMenuPage = new ItalianoMenu();
+            this.Content = ItalianoMenuPage;
+        }
+
+
+        private void Button_Click2(object sender, RoutedEventArgs e)
+        {
+            JapaneseMenu JapaneseMenuPage = new JapaneseMenu();
+            this.Content = JapaneseMenuPage;
+        }
+
+
+        private void Button_Click3(object sender, RoutedEventArgs e)
+        {
+            Visitors VisitorsPage = new Visitors();
+            this.Content = VisitorsPage;
+        }
+
+
+        private void Button_Click4(object sender, RoutedEventArgs e)
+        {
+            Waiter WaiterPage = new Waiter();
+            this.Content = WaiterPage;
+        }
+
+    }
+}
---
+++ zapupa/Visitors.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace zapupa
+{
+   
+    public partial class Visitors : Page
+    {
+        private IL_PATIOEntities entities = new IL_PATIOEntities();
+        public Visitors()
+        {
+            InitializeComponent();
+            ClientDateGrid.ItemsSource = entities.VISITOR.ToList();
+        }
+    }
+}
